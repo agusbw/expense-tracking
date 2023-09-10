b2ddba816c98c09461ed9d4b710a9d17771a697a
agusbw <agusbudhiarta81@gmail.com>
2023-09-10 19:59:34 +0800

    chore: delete unused files

--- app/(general)/dashboard/history/page.tsx
+++
@@ -1,4 +0,0 @@
-const page = () => {
-  return <div>Hehehe</div>;
-};
-export default page;
--- app/(general)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "@/app/(general)/_components/hero";
-
-export default function Home() {
-  return (
-    <section className="flex items-center">
-      <Hero />
-    </section>
-  );
-}
